2cba4f12a80558ca3e816370f090cb6205803381
amy97ye <amye@ucdavis.edu>
2017-03-11 22:27:20 -0800

    yayayay

--- project/proto3/script.js
+++ project/proto3/script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var signInButton = document.getElementById("sign-in-button");
     var signOutButton = document.getElementById("sign-out-button");
+    var currentUser = document.getElementById("current-user");
 
     signInButton.addEventListener('click', function(){
 
@@ -12,5 +13,29 @@ document.addEventListener("DOMContentLoaded", function() {
       firebase.auth().signInWithRedirect(provider);
 
     });
+
+    signOutButton.addEventListener('click', function(){
+
+      firebase.auth().signOut();
+
+    });
+
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+
+        currentUser.innerHTML(user.displayName);
+      } else {
+
+        currentUser.innerHTML("null");
+  }
+});
 
 });
